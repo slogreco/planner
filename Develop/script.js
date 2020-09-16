ee2348bca31c3f79acc06123e1b57ce7b90e40b5
Slogreco <shoshwright@hotmail.com>
2020-09-16 06:48:51 -0700

    save button click event created

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,5 @@
 let time = ["8:00AM", "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
 
+$(".saveBtn").on("click", function(){
+console.log("button was clicked")
+});
