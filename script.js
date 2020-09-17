789c96d791fdb4075825cc3c26a8aadd5b54cb1c
Slogreco <shoshwright@hotmail.com>
2020-09-16 21:25:10 -0700

    get item from local storage

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ $("#currentDay").append(date);
 
 
 
-let timeOfday = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
+let time = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
 updatetime();
 
 function updatetime() {
-    var currentTime = moment().format('H');
-    for (var i = 0; i < timeOfday.length; i++) {
+    let currentTime = moment().format('H');
+    for (var i = 0; i < time.length; i++) {
 
-        if (parseInt(timeOfday[i]) > currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
+        if (parseInt(time[i]) > currentTime) {
+            $("#" + time[i]).attr("style", "background-color: #58ce7b");
 
-
-        }
-        else if (parseInt(timeOfday[i]) < currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
 
         }
-        else if (parseInt(timeOfday[i]) == currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
+        else if (parseInt(time[i]) < currentTime) {
+            $("#" + time[i]).attr("style", "background-color: lightgray");
+
+        }
+        else if (parseInt(time[i]) == currentTime) {
+            $("#" + time[i]).attr("style", "background-color: #fc665e");
 
         }
     }
@@ -35,14 +35,21 @@ function updatetime() {
 
 $(".saveBtn").on("click", function(){
     let timeOfday = $(this).parent().attr("id");
-    let textContent = $("input").val();
-    // console.log(timeOfday);
-    // console.log(textContent);
+    let textContent = $("#time", "input").val();
+    console.log(timeOfday);
+    console.log(textContent);
 
     localStorage.setItem(timeOfday, textContent);
     
 })
 
 $("#8am").children("input").val(localStorage.getItem("8am"));
+$("#9am").children("input").val(localStorage.getItem("9am"));
+$("#10am").children("input").val(localStorage.getItem("10am"));
+$("#11am").children("input").val(localStorage.getItem("11am"));
 $("#12pm").children("input").val(localStorage.getItem("12pm"));
-
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
