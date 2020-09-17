8de809eeb56a3e9fec03dfc17beaf029e38b6d0b
Slogreco <shoshwright@hotmail.com>
2020-09-16 20:08:54 -0700

    added update time function

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
+
+
+
 let date = moment().format("MMMM Do YYYY");
-console.log(date);
+// console.log(date);
 moment().format("LT");
 
 $("#currentDay").append(date);
 
+
+
+let timeOfday = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
+updatetime();
+
+function updatetime() {
+    var currentTime = moment().format('H');
+    for (var i = 0; i < timeOfday.length; i++) {
+
+        if (parseInt(timeOfday[i]) > currentTime) {
+            $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
+
+
+        }
+        else if (parseInt(timeOfday[i]) < currentTime) {
+            $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
+
+        }
+        else if (parseInt(timeOfday[i]) == currentTime) {
+            $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
+
+        }
+    }
+}
 
